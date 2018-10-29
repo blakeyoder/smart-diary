9798afbb91dc3de05287f5a5980d34195676591a
Blake Yoder <yoderblake@gmail.com>
2018-10-29 00:33:05 -0400

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './App.scss';
 
 export default class App extends Component {
   constructor(props) {
@@ -32,19 +33,30 @@ export default class App extends Component {
 
   render = () => {
     return (
-      <div className="App">
-        <h1>Welcome to your day</h1>
-        <textarea
-          onChange={this.handleInputChange}
-        />
-        <div>
-          <button onClick={(e) => this.processInput(e, 'sentiment')}>Get Sentiment</button>
-          <button onClick={(e) => this.processInput(e, 'keyPhrases')}>Get KeyPhrases</button>
+      <React.Fragment>
+          <div className='app__container'>
+          <div className='app__container--left'>
+            <h1 className='app__header'>Welcome to your day</h1>
+          </div>
+          <div className='app__container--right'>
+            <textarea
+              className='app__input'
+              onChange={this.handleInputChange}
+            />
+            <div className='app__button--container'>
+              <button
+                className='app__button'
+                onClick={(e) => this.processInput(e, 'sentiment')}>Get Sentiment</button>
+              <button
+                className='app__button app__button--no-left-border'
+                onClick={(e) => this.processInput(e, 'keyPhrases')}>Get KeyPhrases</button>
+            </div>
+            <div>
+              <p>{this.state.sentiment.Sentiment}</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>{this.state.sentiment.Sentiment}</p>
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
